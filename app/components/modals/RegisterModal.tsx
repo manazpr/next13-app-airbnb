35a60852709523bcdb292138641f080b31814c0a
manazpr <manazpr@gmail.com>
2023-05-07 17:59:35 +0530

    Added button, modal,  state management

---
+++ app/components/modals/RegisterModal.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import axios from 'axios';
+import { AiFillGithub } from 'react-icons/ai';
+import { FcGoogle } from 'react-icons/fc';
+import { useCallback, useState } from 'react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import useRegisterModal from '../../hooks/useRegisterModal';
+
+const RegisterModal = () => {
+  const registerModal = useRegisterModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+  };
+  return <div></div>;
+};
+
+export default RegisterModal;
