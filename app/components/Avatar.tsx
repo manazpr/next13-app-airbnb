f697089a4ddbb967eee684a40160c8a20a63120d
manazpr <manazpr@gmail.com>
2023-05-08 23:24:20 +0530

    feat: added social login (google & github)

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,15 +1,18 @@
 'use client';
 
 import Image from 'next/image';
+interface AvatarProps {
+  src: string | null | undefined;
+}
 
-const Avatar = () => {
+const Avatar: React.FC<AvatarProps> = ({ src }) => {
   return (
     <Image
       className="rounded-full"
       alt="Avatar"
       height="30"
       width="30"
-      src="/images/placeholder.jpg"
+      src={src || '/images/placeholder.jpg'}
     />
   );
 };
