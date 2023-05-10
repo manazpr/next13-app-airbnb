c30116722bcd09febcc989efc3a5d49e7cd6ac4c
manazpr <manazpr@gmail.com>
2023-05-10 21:31:20 +0530

    feat: added listing card, fetching listings

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,9 @@ export default async function RootLayout({
         <RegisterModal />
         <RentModal />
         <Navbar currentUser={currentUser} />
+        <div className="pb-20 pt-28">
         {children}
+        </div>
       </body>
     </html>
   );
