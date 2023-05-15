2ef0d8c894236ee0adbcbfe7d75b42170c90ab07
manazpr <manazpr@gmail.com>
2023-05-15 19:05:44 +0530

    feat: changed currency

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -26,7 +26,7 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
         className='bg-white rounded-xl border-[1px] border-neutral-200 overflow-hidden'>
       <div className='flex flex-row items-center gap-1 p-4'>
         <div className='text-2xl font-semibold'>
-            $ {price}
+        ₹ {price}
         </div>
         <div className='font-light text-neutral-600'>
             night
@@ -47,7 +47,7 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
       </div>
       <div className='flex flex-row items-center justify-between p-4 text-lg font-semibold'>
         <div>Total</div>
-        <div>$ {totalPrice}</div>
+        <div> {totalPrice}</div>
       </div>
     </div> );
 }
